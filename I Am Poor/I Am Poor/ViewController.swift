504b7d21d6d4644178993259b98ab063dc76c4df
bmoyers2021 <60445009+bmoyers2021@users.noreply.github.com>
2020-02-06 11:08:17 -0500

    Add files via upload
    homework #1 submitted

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Beatrice Moyers on 2/3/20.
+//  Copyright © 2020 Beatrice Moyers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
